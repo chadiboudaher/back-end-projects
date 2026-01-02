95d20319be7b698db4547c04f21ef37eb20fd1f4
chadiboudaher <chadiboudaher7@gmail.com>
2026-01-02 16:02:03 +0200

    Implementing CRUD

--- basic-backend/server.js
+++ basic-backend/server.js
@@ -3,14 +3,20 @@ const express = require("express");
 const app = express(); // create a backend application
 const PORT = 8383;
 
-let data = {
-  name: "james",
-};
+let data = ["james"];
+
+app.use(express.json()); // configure to expect json data
 
 // Website enpoints (For sending back an HTML)
 
 app.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1>");
+  res.send(`
+    <body 
+    style="background:pink;
+    color:blue">
+      <p>${JSON.stringify(data)}</p>
+    </body>
+    `);
 });
 
 app.get("/dashboard", (req, res) => {
@@ -19,9 +25,25 @@ app.get("/dashboard", (req, res) => {
 
 // TYPE 2 - API endpoints (When submittion happen)
 
+// CRUD - read-get create-post update-put delete-delete
+
 app.get("/api/data", (req, res) => {
   console.log("This one was for data");
   res.send(data);
+});
+
+app.post("/api/data", (req, res) => {
+  // someone want to create a user
+  const newEntry = req.body;
+  console.log(newEntry);
+  data.push(newEntry.name);
+  res.sendStatus(201);
+});
+
+app.delete("/api/data", (req, res) => {
+  data.pop();
+  console.log("We deleted the last element of the array");
+  res.sendStatus(203);
 });
 
 app.listen(PORT, () => {
